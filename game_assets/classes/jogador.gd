751ee19d9fa96757835639f8bd478cf525c52b55
João Gabriel Lima Marinho <jg.limamarinho202@gmail.com>
2025-06-24 00:28:11 -0300

    Add game flow manager and update game scene layout
    - Introduced a new script for managing game flow (`gerenciador_de_fluxo_jogo.gd`) that initializes bot players and adds them to the GUI.
    - Updated `game_scene.tscn` to reflect new layout changes, including restructuring GUI elements and adding new nodes for better organization.
    - Adjusted positioning and properties of various UI components to enhance visual presentation and functionality.

---
+++ game_assets/classes/jogador.gd
@@ -0,0 +1,20 @@
+class_name Jogador extends Node2D
+
+
+var nome: String = "Jogador BOT"
+var isBot: bool = false
+var trens: int = 45
+var cor: int = 0
+var cartas: Array[GameCard] = []
+var cartas_destino = []
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
